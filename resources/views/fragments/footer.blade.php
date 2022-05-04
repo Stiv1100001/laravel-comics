d1670a8fdf831fcc497842217565f5e8be47648b
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-05-04 13:50:40 +0200

    Started project

---
+++ resources/views/fragments/footer.blade.php
@@ -0,0 +1,29 @@
+<footer>
+    <div id="footer-up">
+        <div class="container">
+            <div class="url-container">
+                {{-- <ul v-for="(link, index) in links" :key="index">
+                    <li v-for="(url, index2) in link" :key="index2">
+                        <a href="#"> {{ url }}</a>
+                    </li>
+                </ul> --}}
+            </div>
+            <img src="images/dc-logo-bg.png" alt="" />
+        </div>
+    </div>
+    <div id="footer-down">
+        <div class="container">
+            <a href="#" class="btn"> Sign-up now!</a>
+            <div>
+                <span>follow us!</span>
+                {{-- <ul>
+                    <li v-for="(img, index) in social" :key="index">
+                        <a href="#">
+                            <img :src="`/img/${img}`" alt="social icon" />
+                        </a>
+                    </li>
+                </ul> --}}
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/templates/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+
+@include('fragments.head')
+
+<body>
+    @include('fragments.header')
+
+    @yield('main-content')
+
+    @include('fragments.footer')
+</body>
+
+</html>
