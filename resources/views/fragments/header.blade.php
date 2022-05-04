04a4542d42c03b52b1fc213feaef67222eacb6d3
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-05-04 14:51:36 +0200

    header, main done

--- resources/views/fragments/header.blade.php
+++ resources/views/fragments/header.blade.php
@@ -1,1 +1,29 @@
-<header></header>
+<header>
+    <div class="bg-white">
+        <div class="bg-blue">
+            <div class="container d-flex justify-content-end align-items-center">
+                <span class="me-5">DC POWERED VISA</span>
+                <span>ADDITIONAL DC SITES</span>
+            </div>
+        </div>
+        <div class="container">
+            <div class="header">
+                <img src="images/dc-logo.png" alt="DC Logo" />
+                <nav>
+                    <ul>
+                        <li><a href="#">characters</a></li>
+                        <li class="active"><a href="#">comics</a></li>
+                        <li><a href="#">movie</a></li>
+                        <li><a href="#">tv</a></li>
+                        <li><a href="#">games</a></li>
+                        <li><a href="#">collectibles</a></li>
+                        <li><a href="#">videos</a></li>
+                        <li><a href="#">fans</a></li>
+                        <li><a href="#">news</a></li>
+                        <li><a href="#"">shop</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,25 @@
 @extends('templates.default')
 
 @section('title', 'Home')
+
+@section('main-content')
+<main>
+    <div class="bg-black">
+        <div id="jumbotron"></div>
+        <div class="container">
+            <h1 class="title">Current Series</h1>
+            <div class="cards">
+                @foreach ($comics as $comic)
+                <div class="card bg-black">
+                    <img src="{{ $comic['thumb']}}" alt="">
+                    <h3>{{$comic[ 'title']}}</h3>
+                </div>
+                @endforeach
+            </div>
+            <div class="buttons">
+                <button class="btn">Load MOre</button>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
